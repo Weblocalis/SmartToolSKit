a7339a4ea1df788a2c2cadc35328c70e3ac4f642
Mharzi <abdelhakmharzi@gmail.com>
2024-12-12 16:22:20 +0100

    df

--- admin/js/generate-sitemap.js
+++ admin/js/generate-sitemap.js
@@ -1,10 +1,8 @@
 const fs = require('fs');
 const path = require('path');
+const chokidar = require('chokidar');
+const utilDirectory = path.join(__dirname, '../../utilitaire');  // Répertoire avec vos fichiers HTML
 
-// Répertoire des fichiers HTML
-const utilDirectory = path.join(__dirname, '../../utilitaire');  // Remplacez par votre répertoire
-
-// Fonction pour lister tous les fichiers HTML dans un répertoire
 function getHTMLFiles(dir) {
   let files = fs.readdirSync(dir);
   let htmlFiles = [];
@@ -14,19 +12,18 @@ function getHTMLFiles(dir) {
     const stat = fs.statSync(filePath);
 
     if (stat.isDirectory()) {
-      htmlFiles = htmlFiles.concat(getHTMLFiles(filePath));  // Recherche récursive dans les sous-répertoires
+      htmlFiles = htmlFiles.concat(getHTMLFiles(filePath)); // Recherche récursive
     } else if (file.endsWith('.html')) {
-      htmlFiles.push(filePath.replace(__dirname, '').replace(/\\/g, '/'));  // Normalisation des chemins
+      htmlFiles.push(filePath.replace(__dirname, '').replace(/\\/g, '/'));
     }
   });
 
   return htmlFiles;
 }
 
-// Générer le contenu du sitemap
 function generateSitemap() {
   const htmlFiles = getHTMLFiles(utilDirectory);
-  const urlBase = 'https://votresite.github.io';  // Remplacez par l'URL de votre site GitHub Pages
+  const urlBase = 'https://votresite.github.io';  // Remplacez par l'URL de votre GitHub Pages
   let sitemapContent = '<?xml version="1.0" encoding="UTF-8"?>\n';
   sitemapContent += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
 
@@ -40,8 +37,14 @@ function generateSitemap() {
 
   // Sauvegarder le fichier sitemap.xml
   fs.writeFileSync(path.join(__dirname, '../../sitemap.xml'), sitemapContent);
-  
   console.log('Sitemap généré avec succès!');
 }
 
+// Générer le sitemap initial
 generateSitemap();
+
+// Surveiller les changements dans les fichiers HTML et générer à nouveau le sitemap
+chokidar.watch('../../utilitaire/**/*.html').on('change', (path) => {
+  console.log(`Fichier modifié: ${path}`);
+  generateSitemap();
+});
